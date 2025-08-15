1b61bbced55118559587e2207ad59afb4b071a9f
kaiovbraga <kaiovbraga2001@gmail.com>
2025-08-15 18:44:05 -0300

    fix: Adding ssl paths

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = process.env.PROFILE === 'PROD' && {
+    key: fs.readFileSync(process.env.PRIVKEY_PATH),
+    cert: fs.readFileSync(process.env.FULLCHAIN_PATH),
+  };
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
 
   app.useGlobalPipes(
     new ValidationPipe({
